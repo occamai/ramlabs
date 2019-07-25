912d36489b7f3b403fbd84b1bce488748d60fa33
gosha1128 <george.williams@gmail.com>
2019-07-24 18:52:42 -0700

    gw/gsiventurelabs

--- app/containers/Home/data.js
+++ app/containers/Home/data.js
@@ -1,11 +1,25 @@
+import data_1_a from '../../images/RAM-e1518601759565-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png';
+import data_1_b from '../../images/Chip-e1518601899365-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png';
+import data_2_a from '../../images/Perfomance-e1518601974872-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png';
+import data_2_b from '../../images/SRAM_size_reduction-1-e1518602441369-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png';
+import data_2_c from '../../images/Secure-e1518602037890-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png';
+import data_3_a from '../../images/rsz_iot_mordern_cityscape.jpg';
+import data_3_b from '../../images/ai_robot.jpg';
+import data_4_a from '../../images/rsz_datacenter_server_room.jpg';
+import data_4_b from '../../images/PgIsmkx.jpg'; //'../../images/autonomous_vehicle-1024x683.jpg';
+import data_4_c from '../../images/face.png'; //images/rsz_digital_drone.jpg';
+
 export const DATA_1 = [
   {
-    img: 'http://www.numem.com/wp-content/uploads/elementor/thumbs/RAM-e1518601759565-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png',
+    img: data_1_a,
+    //img: 'http://www.numem.com/wp-content/uploads/elementor/thumbs/RAM-e1518601759565-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png',
     title: 'Advanced In-Memory Technology',
-    description: 'A new class of SRAM-based in-memory processors are on the verge of disrupting the semiconductor industry by enabling deep integration of memory and compute. At 28nm, our new Gemini processors will revolutionize the general computing landscaope.'
+    description: 'A new class of processor-in-SRAM technology are on the verge of disrupting the semiconductor industry. These new associative memories deeply integrate high-performance SRAM with programmable bit-logic at 28nm.'
   },
+
   {
-    img: 'http://www.numem.com/wp-content/uploads/elementor/thumbs/Chip-e1518601899365-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png',
+    img: data_1_b,
+    //img: 'http://www.numem.com/wp-content/uploads/elementor/thumbs/Chip-e1518601899365-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png',
     title: 'The Team at GSI Venture Labs',
     description: 'The team at GSI has been working on high performance SRAM technologies for last 20 years and have taped out multiple chips. GSI Venture Labs leverages its patented technology to provide best in class performance and product manufacturing robustness.'
   },
@@ -13,27 +27,33 @@ export const DATA_1 = [
 
 export const DATA_2 = [
   {
-    img: 'http://www.numem.com/wp-content/uploads/elementor/thumbs/Perfomance-e1518601974872-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png',
+    img: data_2_a,
+    //img: 'http://www.numem.com/wp-content/uploads/elementor/thumbs/Perfomance-e1518601974872-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png',
     title: 'Benefits over CPU',
   },
   {
-    img: 'http://www.numem.com/wp-content/uploads/elementor/thumbs/SRAM_size_reduction-1-e1518602441369-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png',
+    img: data_2_b,
+    //img: 'http://www.numem.com/wp-content/uploads/elementor/thumbs/SRAM_size_reduction-1-e1518602441369-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png',
     title: 'Benefits over GPU',
   },
   {
-    img: 'http://www.numem.com/wp-content/uploads/elementor/thumbs/Secure-e1518602037890-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png',
+    img: data_2_c,
+    //img: 'http://www.numem.com/wp-content/uploads/elementor/thumbs/Secure-e1518602037890-no5fx2j0hnpezo5l54hrjzg1usqhw6yt4b2055cpgo.png',
     title: 'Ready For Space',
   },
 ];
 
 export const DATA_3 = [
   {
-    img: 'http://www.numem.com/wp-content/uploads/2018/02/rsz_iot_mordern_cityscape.jpg',
+    img: data_3_a,
+    //img: 'http://www.numem.com/wp-content/uploads/2018/02/rsz_iot_mordern_cityscape.jpg',
     title: 'Billion-Scale Similarity Search',
     description: 'The industry’s leading large-scale similarity search technology stack'
   },
   {
-    img: 'http://www.numem.com/wp-content/uploads/2018/02/ai_robot.jpg',
+    img: data_3_b,
+    //img: 'http://www.numem.com/wp-content/uploads/2018/02/ai_robot.jpg',
+    //img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLc23vSc4LRlTGa4cCtSr4LL7vvEa3kRhL6d0Kcsy1k2X4tTT9',
     title: 'Operationalized AI',
     description: 'Integrate deep learning with high capacity search.'
   },
@@ -41,19 +61,23 @@ export const DATA_3 = [
 
 export const DATA_4 = [
   {
-    img: 'http://www.numem.com/wp-content/uploads/2018/02/rsz_datacenter_server_room.jpg',
+    img: data_4_a,
+    //img: 'http://www.numem.com/wp-content/uploads/2018/02/rsz_datacenter_server_room.jpg',
+    //img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi72jjERl8KHKKH9oXuQ4-bWedrqWmTP8GqamFclM1lsat9hLu',
     title: 'Data Center/Server',
     description: 'The best data server accelerators out there with high bandwidth and low latency.'
   },
   {
+    img: data_4_b,
     //img: 'http://www.numem.com/wp-content/uploads/2018/02/autonomous_vehicle-1024x683.jpg',
-    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiHkKkXu6FgrFAbLQAuFqlbKxcSbe1kh8s9kEn63x2oCDEbn9Q',
-    title: 'Chem and Bioinformatics',
+    //img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiHkKkXu6FgrFAbLQAuFqlbKxcSbe1kh8s9kEn63x2oCDEbn9Q',
+    title: 'Chem/Bioinformatics',
     description: 'Advanced, low latency tools for computational drug discovery.'
   },
   {
-    img: 'http://www.numem.com/wp-content/uploads/2018/02/rsz_digital_drone.jpg',
-    title: 'Biometrics and Surveillance',
-    description: 'Software security and quality for Cameras.'
+    img: data_4_c,
+    //img: 'http://www.numem.com/wp-content/uploads/2018/02/rsz_digital_drone.jpg',
+    title: 'Biometrics/Surveillance',
+    description: 'State-of-the-art face recogntion and video search.'
   },
 ];
